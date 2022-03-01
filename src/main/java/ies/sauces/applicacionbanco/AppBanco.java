c75d017f2e7c3143eac6a764ac5f925c49064c54
Ricardo <ricardo.santom@educa.jcyl.es>
2022-03-01 18:37:30 +0100

    Commit inicial applicacionBanco

---
+++ src/main/java/ies/sauces/applicacionbanco/AppBanco.java
@@ -0,0 +1,238 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ies.sauces.applicacionbanco;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Ricardo Santiago Tomé
+ *
+ */
+public class AppBanco {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Banco banco = new Banco("Banco Sauces");
+        int opcion = 0, opcion2 = 0;
+        Scanner teclado = new Scanner(System.in);
+        List<Cuenta> listado;
+        Cuenta cuenta1, cuenta2;
+        CuentaCredito cc;
+        boolean correcto;
+        //casting
+        /*((CuentaCredito)cuenta1).getLimiteCredito()*/
+
+        for (TipoMovimiento tp : TipoMovimiento.values()) {
+            System.out.println(tp);
+        }
+
+        do {
+            System.out.println("1.Abrir cuenta");
+            System.out.println("2.Operar cuenta");
+            System.out.println("3.Cancelar cuenta");
+            System.out.println("4.Listar cuentas");
+            System.out.println("5.Consultar total depositos");
+            System.out.println("0.Salir");
+            System.out.println("Introduzca opcion:");
+            do {
+                correcto = true;
+                try {
+                    opcion = teclado.nextInt();
+                } catch (InputMismatchException ime) {
+                    correcto = false;
+                    System.out.println("Revise formato.(1-5) o (0)");
+                } finally {
+                    teclado.nextLine();
+                }
+            } while (!correcto);
+            /*while (!teclado.hasNextInt()) {
+                teclado.nextLine();
+            }
+            opcion = teclado.nextInt();
+            teclado.nextLine();*/
+            switch (opcion) {
+                case 1:
+                    String titular,
+                     codigo;
+                    float saldo;
+                    try {
+                        System.out.println("1.Abrir cuenta");
+                        codigo = pedirCodigo("Introduzca codigo de cuenta:");
+                        System.out.println("Introduzca titular de la cuenta");
+                        titular = teclado.nextLine();
+                        System.out.println("Introduzca saldo inicial.");
+                        saldo = teclado.nextFloat();
+                        if (banco.abrirCuenta(codigo, titular, saldo)) {
+                            System.out.println("Cuenta creada");
+                        } else {
+                            System.out.println("No se ha podido crear la cuenta");
+                        }
+                    } catch (SaldoException ex) {
+                        System.out.println(ex.getMessage());
+                    } catch (InputMismatchException ime) {
+                        System.out.println("Revise valor.");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("2.Operar cuenta");
+                    codigo = pedirCodigo("Introduzca codigo de cuenta para operar:");
+                    cuenta1 = banco.getCuenta(codigo);
+                    if (cuenta1 != null) {
+                        do {
+                            System.out.println("1.Ingresar dinero");
+                            System.out.println("2.Retirar dinero");
+                            System.out.println("3.Consultar saldo");
+                            System.out.println("4.Realizar transferencia");
+                            System.out.println("5.Consultar movimientos");
+                            System.out.println("0.Atrás.");
+                            System.out.println("Introduzca opcion:");
+                            do {
+                                correcto = true;
+                                try {
+                                    opcion2 = teclado.nextInt();
+                                } catch (InputMismatchException ime) {
+                                    correcto = false;
+                                    System.out.println("Revise formato.(1-5) o (0)");
+                                } finally {
+                                    teclado.nextLine();
+                                }
+                            } while (!correcto);
+                            switch (opcion2) {
+                                case 1:
+                                    float cantidad;
+                                    System.out.println("1.Ingresar dinero");
+                                    cantidad = pedirFloat("Introduzca cantidad a ingresar");
+                                    cuenta1.ingresar(cantidad);
+                                    System.out.println("Cantidad ingresada. Saldo=" + cuenta1.getSaldo());
+                                    break;
+                                case 2:
+                                    System.out.println("2.Retirar dinero");
+                                    System.out.println("Introduzca cantidad a reintegrar.");
+                                    try {
+                                        cantidad = teclado.nextFloat();
+                                        cuenta1.reintegrar(cantidad);
+                                        System.out.println("Saldo=" + cuenta1.getSaldo());
+                                    } catch (SaldoInsuficienteException sie) {
+                                        System.out.println(sie.getMessage());
+                                    } catch (IllegalArgumentException iae) {
+                                        System.out.println(iae.getMessage());
+                                    } catch (NullPointerException nue) {
+                                        System.out.println("Argumento no valido.");
+                                    }catch(InputMismatchException ime){
+                                        System.out.println("Revise formato.(1-5) o (0)");
+                                    } 
+                                    catch (SaldoException se) {
+                                        System.out.println(se.getMessage());
+                                    }
+
+                                    System.out.println("Saldo actual de la cuenta=" + cuenta1.getSaldo());
+                                    break;
+
+                                case 3:
+                                    System.out.println("3.Consultar saldo");
+                                    System.out.println(cuenta1.getSaldo());
+                                    break;
+                                case 4:
+                                    System.out.println("4.Realizar transferencia");
+                                    codigo = pedirCodigo("Introduzca codigo de cuenta destino:");
+                                    cuenta2 = banco.getCuenta(codigo);
+                                    if (cuenta2 != null) {
+                                        cantidad = pedirFloat("Introduzca cantidad a transferir");
+                                        try {
+                                            cuenta1.realizarTransferencia(cuenta2, cantidad);
+                                        } catch (SaldoException ex) {
+                                            System.out.println(ex.getMessage());
+                                        }
+                                    } else {
+                                        System.out.println("Error en la transferencia");
+                                    }
+                                    System.out.println("Saldo =" + cuenta1.getSaldo());
+                                    System.out.println("mensaje prueba");
+                                    teclado.nextLine();
+                                    break;
+                                case 5:
+                                    System.out.println("5.Consultar movimientos");
+                                    codigo = pedirCodigo("Introduzca codigo de cuenta para consultar movimientos.");
+                                    System.out.println();
+                                    if (cuenta1.getCodigo().equals(codigo)) {
+                                        System.out.println(cuenta1.listarMovimientos());
+                                    } else {
+                                        System.out.println("No hay una cuenta con este código.");
+                                    }
+                                    break;
+                                case 0:
+                                    System.out.println("Hasta pronto.");
+                                    break;
+                                default:
+                                    System.out.println("Opción incorrecta.");
+                            }
+                        } while (opcion2 != 0);
+                    } else {
+                        System.out.println("No existe una cuenta con ese código.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("3.Cancelar cuenta");
+                    codigo = pedirCodigo("Introduzca codigo de cuenta");
+                    if (banco.cancelarCuenta(codigo)) {
+                        System.out.println("Cuenta cancelada");
+                    }
+                    break;
+                case 4:
+                    System.out.println("4.Listar cuentas");
+                    listado = banco.getCuentas();
+                    for (Cuenta c : listado) {
+                        System.out.println(c);
+                    }
+                    System.out.println();
+                    break;
+                case 5:
+                    System.out.println("5.Consultar total depositos");
+                    System.out.println("Total depósitos:" + banco.getTotalDepositos());
+                    break;
+                case 0:
+                    System.out.println("Hasta pronto.");
+                    break;
+                default:
+                    System.out.println("Opción incorrecta.");
+            }
+
+        } while (opcion != 0);
+    }
+
+    private static String pedirCodigo(String mensaje) {
+        Scanner teclado = new Scanner(System.in);
+        String codigo;
+        do {
+            System.out.println(mensaje);
+            codigo = teclado.nextLine();
+        } while (codigo.length() <= 0);
+        return codigo;
+    }
+
+    private static float pedirFloat(String mensaje) {
+        Scanner teclado = new Scanner(System.in);
+        float n;
+        System.out.println(mensaje);
+        while (!teclado.hasNextFloat()) {
+            teclado.nextLine();
+        }
+        n = teclado.nextFloat();
+        teclado.nextLine();
+        return n;
+    }
+
+}
---
+++ src/main/java/ies/sauces/applicacionbanco/CuentaCredito.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ies.sauces.applicacionbanco;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author daw1
+ */
+public class CuentaCredito extends Cuenta{
+    private float limiteCredito;
+
+    public CuentaCredito(String codigo, String titular, float saldo,float limiteCredito) throws SaldoException {
+        super(codigo, titular, saldo);
+        this.limiteCredito = limiteCredito;
+    }
+
+    public float getLimiteCredito() {
+        return limiteCredito;
+    }
+
+    public void setLimiteCredito(float limiteCredito) {
+        this.limiteCredito = limiteCredito;
+    }
+
+    @Override
+    public void reintegrar(float cantidad) {
+        float nuevoSaldo;
+        if(cantidad >= 0 ){
+            nuevoSaldo=getSaldo()-cantidad;
+            if(nuevoSaldo>=limiteCredito){
+                try {
+                    setSaldo(nuevoSaldo);
+                } catch (SaldoException ex) {
+                    System.out.println("No se ha podido hacer el reintegro.");
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+","+limiteCredito;
+    }
+
+    
+}
